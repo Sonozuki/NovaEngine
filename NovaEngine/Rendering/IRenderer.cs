84991cf75269634a666dbab313f72a30d5d75b87
Andy Crocker <atmcrocker@gmail.com>
2021-04-11 16:15:08 +0100

    Add renderer classes

---
+++ NovaEngine/Rendering/IRenderer.cs
@@ -0,0 +1,33 @@
+﻿using NovaEngine.Maths;
+using System;
+
+namespace NovaEngine.Rendering
+{
+    /// <summary>Represents a graphics renderer.</summary>
+    public interface IRenderer
+    {
+        /*********
+        ** Accessors
+        *********/
+        /// <summary>Whether the renderer can be used on the current environment platform.</summary>
+        public bool CanUseOnPlatform { get; }
+
+
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>Invoked when the renderer should initialise itself.</summary>
+        /// <param name="windowHandle">The handle to the application window.</param>
+        public void OnInitialise(IntPtr windowHandle);
+
+        /// <summary>Invoked when the window gets resized.</summary>
+        /// <param name="newSize">The new size of the window.</param>
+        public void OnWindowResize(Size newSize);
+
+        /// <summary>Invoked when the renderer should render a frame.</summary>
+        public void OnRenderFrame();
+
+        /// <summary>Invoked when the renderer should clean itself up.</summary>
+        public void OnCleanUp();
+    }
+}
---
+++ NovaEngine/Rendering/RendererManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace NovaEngine.Rendering
+{
+    /// <summary>Handles using difference renderers.</summary>
+    public abstract class RendererManager
+    {
+        /*********
+        ** Accessors
+        *********/
+        /// <summary>The renderer that is currently being used.</summary>
+        public static IRenderer CurrentRenderer { get; }
+
+
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>Initialises the class.</summary>
+        static RendererManager()
+        {
+            // get the current platform object
+            var rendererFiles = Directory.GetFiles(Environment.CurrentDirectory, "NovaEngine.Renderer.*.dll");
+            var types = rendererFiles
+                .Select(rendererFile => Assembly.LoadFile(rendererFile))
+                .SelectMany(assembly => assembly.GetExportedTypes())
+                .Where(type => type.IsClass && !type.IsAbstract);
+
+            foreach (var type in types)
+            {
+                // ensure type is a platform
+                if (!(type.GetInterfaces().Contains(typeof(IRenderer))))
+                    continue;
+
+                // try to create an instance
+                var renderer = (IRenderer?)Activator.CreateInstance(type);
+                if (renderer == null)
+                    continue;
+
+                // set to the current renderer if it's usable
+                if (!renderer.CanUseOnPlatform)
+                    continue;
+
+                CurrentRenderer = renderer;
+            }
+
+            if (CurrentRenderer == null)
+                throw new PlatformNotSupportedException("The environment operating system doesn't have a supported renderer.");
+        }
+    }
+}
