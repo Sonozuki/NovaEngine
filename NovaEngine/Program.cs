976e3c16a792598b6875450ab0b2ce99056d54c0
Andy Crocker <atmcrocker@gmail.com>
2021-04-11 15:36:10 +0100

    Add initial project files

---
+++ NovaEngine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NovaEngine
+{
+    /// <summary>The application entry point.</summary>
+    public static class Program
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>The application entry point.</summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
