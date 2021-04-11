f94f850da2760cdfbfe64e17ac1f0b8c33c8d058
Andy Crocker <atmcrocker@gmail.com>
2021-04-11 16:30:28 +0100

    Add initial Vulkan renderer

---
+++ NovaEngine.Renderer.Vulkan/VulkanRenderer.cs
@@ -0,0 +1,43 @@
+﻿using NovaEngine.Maths;
+using NovaEngine.Rendering;
+using System;
+
+namespace NovaEngine.Renderer.Vulkan
+{
+    /// <summary>Represents a graphics renderer using Vulkan.</summary>
+    public class VulkanRenderer : IRenderer, IDisposable
+    {
+        /*********
+        ** Accessors
+        *********/
+        /// <inheritdoc/>
+        public bool CanUseOnPlatform => OperatingSystem.IsWindows();
+
+
+        /*********
+        ** Public Methods
+        *********/
+        /// <inheritdoc/>
+        public void OnInitialise(IntPtr windowHandle)
+        {
+        }
+        
+        /// <inheritdoc/>
+        public void OnWindowResize(Size newSize)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void OnRenderFrame()
+        {
+        }
+
+        /// <inheritdoc/>
+        public void OnCleanUp() => Dispose();
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+        }
+    }
+}
