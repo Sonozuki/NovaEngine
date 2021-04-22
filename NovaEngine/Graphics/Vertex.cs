5e62e9a1f30e24253b580ded1a27481c15afaa62
Andy Crocker <atmcrocker@gmail.com>
2021-04-22 01:41:37 +0100

    Add vertex structure

---
+++ NovaEngine/Graphics/Vertex.cs
@@ -0,0 +1,35 @@
+﻿using NovaEngine.Maths;
+
+namespace NovaEngine.Graphics
+{
+    /// <summary>Represents a mesh vertex.</summary>
+    public struct Vertex
+    {
+        /*********
+        ** Fields
+        *********/
+        /// <summary>The position of the vertex.</summary>
+        public Vector3 Position;
+
+        /// <summary>The texture coordinates of the vertex.</summary>
+        public Vector3 TextureCoordinates;
+
+        /// <summary>The normal of the vertex.</summary>
+        public Vector3 Normal;
+
+
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>Constructs an instance.</summary>
+        /// <param name="position">The position of the vertex.</param>
+        /// <param name="textureCoordinates">The texture coordinates of the vertex.</param>
+        /// <param name="normal">The normal of the vertex.</param>
+        public Vertex(Vector3 position = default, Vector3 textureCoordinates = default, Vector3 normal = default)
+        {
+            Position = position;
+            TextureCoordinates = textureCoordinates;
+            Normal = normal;
+        }
+    }
+}
