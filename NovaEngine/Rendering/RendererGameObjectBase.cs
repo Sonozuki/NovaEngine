3bc6ba3e49378450e2af947aeb563ff16ac76493
Andy Crocker <atmcrocker@gmail.com>
2021-05-08 01:39:38 +0100

    Fix documentation header in RendererGameObjectBase

--- NovaEngine/Rendering/RendererGameObjectBase.cs
+++ NovaEngine/Rendering/RendererGameObjectBase.cs
@@ -28,16 +28,12 @@ namespace NovaEngine.Rendering
         /// <param name="vertices">The vertices of the new mesh.</param>
         /// <param name="indices">The indices of the new mesh.</param>
         public abstract void UpdateMesh(Vertex[] vertices, uint[] indices);
-
-        /// <inheritdoc/>
-        public abstract void Dispose();
-
-
-        /*********
-        ** Internal Methods
-        *********/
+        
         /// <summary>Invoked every tick to update the game object for a specified camera, to update a UBO.</summary>
         /// <param name="camera">The camera to update the game object UBO with.</param>
         public abstract void UpdateUBO(Camera camera);
+
+        /// <inheritdoc/>
+        public abstract void Dispose();
     }
 }
