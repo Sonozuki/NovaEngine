9d1112685f9a2c199383f8e1b14c23b701fe9404
Andy Crocker <atmcrocker@gmail.com>
2021-07-20 12:26:06 +0100

    Remove unnecessary return type parameter from deserialiser

--- NovaEngine/Serialisation/Serialiser.cs
+++ NovaEngine/Serialisation/Serialiser.cs
@@ -44,13 +44,12 @@ namespace NovaEngine.Serialisation
         /// <typeparam name="T">The type of the object to deserialise.</typeparam>
         /// <param name="stream">The stream to deserialise the object from.</param>
         /// <returns>The deserialised object.</returns>
-        public static T? Deserialise<T>(Stream stream) => (T?)Deserialise(stream, typeof(T));
+        public static T? Deserialise<T>(Stream stream) => (T?)Deserialise(stream);
 
         /// <summary>Deserialises an object from a stream.</summary>
         /// <param name="stream">The stream to deserialise the object from.</param>
-        /// <param name="returnType">The type of the object to deserialise.</param>
         /// <returns>The deserialised object.</returns>
-        public static object? Deserialise(Stream stream, Type returnType)
+        public static object? Deserialise(Stream stream)
         {
             try
             {
@@ -110,7 +109,7 @@ namespace NovaEngine.Serialisation
                     }
                 }
 
-                return Convert.ChangeType(allObjectInfos.Last().Value, returnType); // the root object is last as the collection was reversed
+                return allObjectInfos.Last().Value; // the root object is last as the collection was reversed
             }
             catch (Exception ex)
             {
