24ead01a91b564c545d9259ec880af10d0031b48
Andy Crocker <atmcrocker@gmail.com>
2021-08-19 01:19:59 +0100

    Add TODO for Exception.Log()

--- NovaEngine/Extensions/ExceptionExtensions.cs
+++ NovaEngine/Extensions/ExceptionExtensions.cs
@@ -13,7 +13,7 @@ namespace NovaEngine.Extensions
         /// <param name="exception">The exception whose message should be logged.</param>
         /// <param name="severity">The severity of create the log as.</param>
         /// <returns>The current instance, used so you can log and throw in a single statement.</returns>
-        public static Exception Log(this Exception exception, LogSeverity severity)
+        public static Exception Log(this Exception exception, LogSeverity severity) // TODO: currently this doesn't get inlined resulting in all logging to happen from the engine (can't use aggressive inlining as that will only work in release)
         {
             Logger.Log(exception.Message, severity);
             return exception;
