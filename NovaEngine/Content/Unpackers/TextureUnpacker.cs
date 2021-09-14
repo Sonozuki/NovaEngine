b61ec9a7b7f6e467108c2a956f9025ae440fbda5
Andy Crocker <atmcrocker@gmail.com>
2021-09-14 17:22:36 +0100

    Fix the texture unpacker

--- NovaEngine/Content/Unpackers/TextureUnpacker.cs
+++ NovaEngine/Content/Unpackers/TextureUnpacker.cs
@@ -1,6 +1,8 @@
 ﻿using NovaEngine.Content.Attributes;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace NovaEngine.Content.Unpackers
 {
@@ -17,8 +19,9 @@ namespace NovaEngine.Content.Unpackers
             using var binaryReader = new BinaryReader(stream);
             var width = binaryReader.ReadInt32();
             var height = binaryReader.ReadInt32();
+            var pixels = MemoryMarshal.Cast<byte, Rgba32>(binaryReader.ReadBytes(width * height * 4));
 
-            using var image = Image.Load(binaryReader.ReadBytes(width * height));
+            using var image = Image.LoadPixelData(pixels.ToArray(), width, height);
             image.SaveAsPng(destinationFile);
         }
     }
