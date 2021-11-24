65bbc8d6d38dfffd88d62e616e03fa1684b8ecfa
Andy Crocker <atmcrocker@gmail.com>
2021-11-24 20:46:56 +0000

    Change Vulkan shader compile.bat to use environment variable

--- NovaEngine.Renderer.Vulkan/Shaders/compile.bat
+++ NovaEngine.Renderer.Vulkan/Shaders/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.2.176.1\Bin32/glslangValidator.exe -V shader.vert -o bin/shader_vert.spv
-C:\VulkanSDK\1.2.176.1\Bin32/glslangValidator.exe -V shader.frag -o bin/shader_frag.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -V shader.vert -o bin/shader_vert.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -V shader.frag -o bin/shader_frag.spv
 pause
