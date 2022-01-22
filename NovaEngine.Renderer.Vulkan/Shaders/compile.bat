d8ab85e9d098d9d1b9c1c616e7ce7dc8afedbe1a
Andy Crocker <atmcrocker@gmail.com>
2022-01-22 16:21:53 +0000

    Update shader compilation to include debug info

--- NovaEngine.Renderer.Vulkan/Shaders/compile.bat
+++ NovaEngine.Renderer.Vulkan/Shaders/compile.bat
@@ -1,7 +1,7 @@
-%VK_SDK_PATH%\Bin\glslangValidator.exe -V generate_frustums.comp -o bin/generate_frustums_comp.spv
-%VK_SDK_PATH%\Bin\glslangValidator.exe -V depth.vert -o bin/depth_vert.spv
-%VK_SDK_PATH%\Bin\glslangValidator.exe -V pbr.vert -o bin/pbr_vert.spv
-%VK_SDK_PATH%\Bin\glslangValidator.exe -V pbr.frag -o bin/pbr_frag.spv
-%VK_SDK_PATH%\Bin\glslangValidator.exe -V solid_colour.vert -o bin/solid_colour_vert.spv
-%VK_SDK_PATH%\Bin\glslangValidator.exe -V solid_colour.frag -o bin/solid_colour_frag.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -g -V generate_frustums.comp -o bin/generate_frustums_comp.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -g -V depth.vert -o bin/depth_vert.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -g -V pbr.vert -o bin/pbr_vert.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -g -V pbr.frag -o bin/pbr_frag.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -g -V solid_colour.vert -o bin/solid_colour_vert.spv
+%VK_SDK_PATH%\Bin\glslangValidator.exe -g -V solid_colour.frag -o bin/solid_colour_frag.spv
 pause
