f724740d82eb5c88b3715b07bcc9f938632b1893
Andy Crocker <atmcrocker@gmail.com>
2022-05-23 12:44:13 +0100

    Improve ICmapFormat.Map documentation

--- NovaEngine/Content/Models/Font/Cmap/ICmapFormat.cs
+++ NovaEngine/Content/Models/Font/Cmap/ICmapFormat.cs
@@ -6,7 +6,7 @@ internal interface ICmapFormat
     /*********
     ** Public Methods
     *********/
-    /// <summary>Maps a character code to a glyph index.</summary>
+    /// <summary>Retrieves the glyph index for a specified characte code.</summary>
     /// <param name="binaryReader">The binary reader; the position isn't reset automatically, so should be handled manually.</param>
     /// <param name="characterCode">The character code.</param>
     /// <returns>The glyph index.</returns>
