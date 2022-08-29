9b414d0417f48099e958e41ef2c0a53f5409a03f
Andy Crocker <atmcrocker@gmail.com>
2022-08-29 21:56:46 +0100

    Add CircularBuffer<T>

---
+++ NovaEngine.Tests/DataStructures/CircularBufferTests.cs
@@ -0,0 +1,364 @@
+﻿namespace NovaEngine.Tests.DataStructures;
+
+/// <summary>The <see cref="CircularBuffer{T}"/> tests.</summary>
+[TestFixture]
+internal class CircularBufferTests
+{
+    /*********
+    ** Public Methods
+    *********/
+    /// <summary>Tests <see cref="CircularBuffer{T}.Size"/>.</summary>
+    [Test]
+    public void Size_BufferContainsLessElementsThanCapacity_ReturnsNumberOfElementsInBuffer()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        Assert.AreEqual(2, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.Size"/>.</summary>
+    [Test]
+    public void Size_BufferIsFull_ReturnsTheCapacity()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        Assert.AreEqual(3, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.IsFull"/>.</summary>
+    [Test]
+    public void IsFull_BufferIsFull_ReturnsTrue()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        Assert.IsTrue(buffer.IsFull);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.IsFull"/>.</summary>
+    [Test]
+    public void IsFull_BufferIsNotFull_ReturnsFalse()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        Assert.IsFalse(buffer.IsFull);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.IsFull"/>.</summary>
+    [Test]
+    public void IsFull_BufferWasFullButAnElementWasPopped_ReturnsFalse()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        buffer.PopFront();
+        Assert.IsFalse(buffer.IsFull);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.IsEmpty"/>.</summary>
+    [Test]
+    public void IsEmpty_BufferIsEmpty_ReturnsTrue()
+    {
+        var buffer = new CircularBuffer<int>(3);
+        Assert.IsTrue(buffer.IsEmpty);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.IsEmpty"/>.</summary>
+    [Test]
+    public void IsEmpty_BufferIsNotEmpty_ReturnsFalse()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1 });
+        Assert.IsFalse(buffer.IsEmpty);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.IsEmpty"/>.</summary>
+    [Test]
+    public void IsEmpty_BufferContainedAnElementThatWasPopped_ReturnsTrue()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1 });
+        buffer.PopFront();
+        Assert.IsTrue(buffer.IsEmpty);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntGet_BufferIsEmpty_ThrowsIndexOutOfRangeException()
+    {
+        var buffer = new CircularBuffer<int>(3);
+        Assert.Throws<IndexOutOfRangeException>(new(() => _ = buffer[0]));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntGet_IndexBetweenZeroAndSize_ReturnsElement()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        Assert.AreEqual(1, buffer[0]);
+        Assert.AreEqual(2, buffer[1]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntGet_IndexLessThanZero_ThrowsIndexOutOfRangeException()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1 });
+        Assert.Throws<IndexOutOfRangeException>(new(() => _ = buffer[-1]));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntGet_IndexMoreThanSize_ThrowsIndexOutOfRangeException()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1 });
+        Assert.Throws<IndexOutOfRangeException>(new(() => _ = buffer[1]));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntSet_BufferIsEmpty_ThrowsIndexOutOfRangeException()
+    {
+        var buffer = new CircularBuffer<int>(3);
+        Assert.Throws<IndexOutOfRangeException>(new(() => buffer[0] = 0));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntSet_IndexBetweenZeroAndSize_SetsElement()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer[0] = 10;
+        buffer[1] = 11;
+        Assert.AreEqual(10, buffer[0]);
+        Assert.AreEqual(11, buffer[1]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntSet_IndexLessThanZero_ThrowsIndexOutOfRangeException()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1 });
+        Assert.Throws<IndexOutOfRangeException>(new(() => buffer[-1] = 0));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}"/>[<see langword="int"/>].</summary>
+    [Test]
+    public void IndexerIntSet_IndexMoreThanSize_ThrowsIndexOutOfRangeException()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1 });
+        Assert.Throws<IndexOutOfRangeException>(new(() => buffer[1] = 0));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}(int, IEnumerable{T})"/>.</summary>
+    [Test]
+    public void ConstructorIntIEnumerable_CapacityLessThanOne_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(new(() => new CircularBuffer<int>(0)));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}(int, IEnumerable{T})"/>.</summary>
+    [Test]
+    public void ConstructorIntIEnumerable_CapacityIsAtLeastOne_SetsCapacity()
+    {
+        var buffer = new CircularBuffer<int>(5);
+        Assert.AreEqual(5, buffer.Capacity);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}(int, IEnumerable{T})"/>.</summary>
+    [Test]
+    public void ConstructorIntIEnumerable_SpecifyNoInitialCollection_BufferIsEmpty()
+    {
+        var buffer = new CircularBuffer<int>(5);
+        Assert.AreEqual(0, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}(int, IEnumerable{T})"/>.</summary>
+    [Test]
+    public void ConstructorIntIEnumerable_InitialCollectionLargerThanCapacity_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(new(() => new CircularBuffer<int>(1, new[] { 1, 2 })));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}(int, IEnumerable{T})"/>.</summary>
+    [Test]
+    public void ConstructorIntIEnumerable_SpecifyInitialCollection_BufferSizeIsInitialCollectionSize()
+    {
+        var buffer = new CircularBuffer<int>(5, new[] { 1, 2, 3 });
+        Assert.AreEqual(3, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}(int, IEnumerable{T})"/>.</summary>
+    [Test]
+    public void ConstructorIntIEnumerable_SpecifyInitialCollection_BufferContainsInitialCollection()
+    {
+        var buffer = new CircularBuffer<int>(5, new[] { 1, 2, 3 });
+        Assert.AreEqual(1, buffer[0]);
+        Assert.AreEqual(2, buffer[1]);
+        Assert.AreEqual(3, buffer[2]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushFront(T)"/>.</summary>
+    [Test]
+    public void PushFront_BufferIsFull_OverridesLastElement()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        buffer.PushFront(10);
+        Assert.AreEqual(10, buffer[0]);
+        Assert.AreEqual(1, buffer[1]);
+        Assert.AreEqual(2, buffer[2]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushFront(T)"/>.</summary>
+    [Test]
+    public void PushFront_BufferIsFull_SizeRemainsCorrect()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        buffer.PushFront(10);
+        Assert.AreEqual(3, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushFront(T)"/>.</summary>
+    [Test]
+    public void PushFront_BufferIsNotFull_AddsElementToTheFront()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PushFront(10);
+        Assert.AreEqual(10, buffer[0]);
+        Assert.AreEqual(1, buffer[1]);
+        Assert.AreEqual(2, buffer[2]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushFront(T)"/>.</summary>
+    [Test]
+    public void PushFront_BufferIsNotFull_IncrementsSize()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PushFront(10);
+        Assert.AreEqual(3, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushBack(T)"/>.</summary>
+    [Test]
+    public void PushBack_BufferIsFull_OverridesFirstElement()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        buffer.PushBack(10);
+        Assert.AreEqual(2, buffer[0]);
+        Assert.AreEqual(3, buffer[1]);
+        Assert.AreEqual(10, buffer[2]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushBack(T)"/>.</summary>
+    [Test]
+    public void PushBack_BufferIsFull_SizeRemainsCorrect()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        buffer.PushBack(10);
+        Assert.AreEqual(3, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushBack(T)"/>.</summary>
+    [Test]
+    public void PushBack_BufferIsNotFull_AddsElementToTheBack()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PushBack(10);
+        Assert.AreEqual(1, buffer[0]);
+        Assert.AreEqual(2, buffer[1]);
+        Assert.AreEqual(10, buffer[2]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PushBack(T)"/>.</summary>
+    [Test]
+    public void PushBack_BufferIsNotFull_IncrementsSize()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PushBack(10);
+        Assert.AreEqual(3, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PopFront()"/>.</summary>
+    [Test]
+    public void PopFront_BufferIsEmpty_ThrowsInvalidOperationException()
+    {
+        var buffer = new CircularBuffer<int>(3);
+        Assert.Throws<InvalidOperationException>(new(() => buffer.PopFront()));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PopFront()"/>.</summary>
+    [Test]
+    public void PopFront_BufferIsNotEmpty_RemovesTheFirstElement()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PopFront();
+        Assert.AreEqual(2, buffer[0]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PopFront()"/>.</summary>
+    [Test]
+    public void PopFront_BufferIsNotEmpty_DecrementsSize()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PopFront();
+        Assert.AreEqual(1, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PopBack()"/>.</summary>
+    [Test]
+    public void PopBack_BufferIsEmpty_ThrowsInvalidOperationException()
+    {
+        var buffer = new CircularBuffer<int>(3);
+        Assert.Throws<InvalidOperationException>(new(() => buffer.PopBack()));
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PopBack()"/>.</summary>
+    [Test]
+    public void PopBack_BufferIsNotEmpty_RemovesTheLastElement()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PopBack();
+        Assert.AreEqual(1, buffer[0]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.PopBack()"/>.</summary>
+    [Test]
+    public void PopBack_BufferIsNotEmpty_DecrementsSize()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.PopBack();
+        Assert.AreEqual(1, buffer.Size);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.Clear()"/>.</summary>
+    [Test]
+    public void Clear_BufferIsCleared()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        buffer.Clear();
+        Assert.AreEqual(0, buffer.Size);
+        Assert.AreEqual(3, buffer.Capacity);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.ToArray()"/>.</summary>
+    [Test]
+    public void ToArray_BufferIsEmpty_ArrayIsEmpty()
+    {
+        var buffer = new CircularBuffer<int>(3);
+        var array = buffer.ToArray();
+        Assert.AreEqual(0, array.Length);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.ToArray()"/>.</summary>
+    [Test]
+    public void ToArray_BufferContainsElements_ArrayContainsCorrectElements()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2 });
+        var array = buffer.ToArray();
+        Assert.AreEqual(1, array[0]);
+        Assert.AreEqual(2, array[1]);
+    }
+
+    /// <summary>Tests <see cref="CircularBuffer{T}.ToArray()"/>.</summary>
+    [Test]
+    public void ToArray_BufferContainsElementsThatWrapAroundToFrontOfInternalArray_ArrayContainsCorrectElements()
+    {
+        var buffer = new CircularBuffer<int>(3, new[] { 1, 2, 3 });
+        buffer.PushFront(10);
+        var array = buffer.ToArray();
+        Assert.AreEqual(10, array[0]);
+        Assert.AreEqual(1, array[1]);
+        Assert.AreEqual(2, array[2]);
+    }
+}
---
+++ NovaEngine/DataStructures/CircularBuffer.cs
@@ -0,0 +1,237 @@
+﻿namespace NovaEngine.DataStructures;
+
+/// <summary>Represents a fixed-length circular buffer.</summary>
+/// <typeparam name="T">THe type of objects the buffer can contain.</typeparam>
+public class CircularBuffer<T> : IEnumerable<T?>
+{
+    /*********
+    ** Fields
+    *********/
+    /// <summary>The internal buffer of the circular buffer.</summary>
+    private readonly T?[] Buffer;
+
+    /// <summary>The index of the first element in the buffer.</summary>
+    private int Start;
+
+    /// <summary>The index of the last element in the buffer.</summary>
+    private int End;
+
+
+    /*********
+    ** Accessors
+    *********/
+    /// <summary>The current number of elements in the buffer.</summary>
+    public int Size { get; private set; }
+
+    /// <summary>The maximum capacity of the buffer.</summary>
+    public int Capacity { get; }
+
+    /// <summary>Whether the buffer is at full capacity.</summary>
+    /// <remarks>If the buffer is at max capacity and an element is pushed, then an element will get removed.</remarks>
+    public bool IsFull => Size == Capacity;
+
+    /// <summary>Whether the buffer is completely empty.</summary>
+    public bool IsEmpty => Size == 0;
+
+    /// <summary>Gets or sets the value at a specified position.</summary>
+    /// <param name="index">The position index.</param>
+    /// <returns>The value at the specified position.</returns>
+    /// <exception cref="IndexOutOfRangeException">Thrown if <paramref name="index"/> is outside of <see cref="Size"/>.</exception>
+    public T? this[int index]
+    {
+        get
+        {
+            if (IsEmpty)
+                throw new IndexOutOfRangeException($"Unable to access index {index}, buffer is empty.");
+            if (index >= Size)
+                throw new IndexOutOfRangeException($"Unable to access index {index}, buffer only contains {Size} element(s).");
+
+            return Buffer[ConvertToInternalIndex(index)];
+        }
+        set
+        {
+            if (IsEmpty)
+                throw new IndexOutOfRangeException($"Unable to access index {index}, buffer is empty.");
+            if (index >= Size)
+                throw new IndexOutOfRangeException($"Unable to access index {index}, buffer only contains {Size} element(s).");
+
+            Buffer[ConvertToInternalIndex(index)] = value;
+        }
+    }
+
+
+    /*********
+    ** Public Methods
+    *********/
+    /// <summary>Constructs an instance.</summary>
+    /// <param name="capacity">The maximum capacity of the buffer.</param>
+    /// <param name="initialContents">The initial contents of the buffer.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="capacity"/> is less than one.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="initialContents"/> contains more elements than is allowed by <paramref name="capacity"/>.</exception>
+    public CircularBuffer(int capacity, IEnumerable<T>? initialContents = null)
+    {
+        if (capacity < 1)
+            throw new ArgumentOutOfRangeException(nameof(capacity), "Must be at least one.");
+
+        var initialContentsArray = initialContents?.ToArray() ?? Array.Empty<T>();
+        if (initialContentsArray.Length > capacity)
+            throw new ArgumentException($"Contains more items than is allowed in the buffer by {nameof(capacity)}", nameof(initialContents));
+
+        Capacity = capacity;
+        Buffer = new T[capacity];
+
+        Array.Copy(initialContentsArray, Buffer, initialContentsArray.Length);
+        Size = initialContentsArray.Length;
+
+        Start = 0;
+        End = Size == capacity ? 0 : Size;
+    }
+
+    /// <summary>Pushes an element to the front of the buffer.</summary>
+    /// <param name="element">The element to add.</param>
+    /// <remarks>If the buffer is full, the element at the end will be popped.</remarks>
+    public void PushFront(T? element)
+    {
+        if (IsFull)
+        {
+            DecrementIndex(ref Start);
+            End = Start;
+            Buffer[Start] = element;
+        }
+        else
+        {
+            DecrementIndex(ref Start);
+            Buffer[Start] = element;
+            Size++;
+        }
+    }
+
+    /// <summary>Pushes an element to the back of the buffer.</summary>
+    /// <param name="element">The buffer to add.</param>
+    /// <remarks>If the buffer is full, the element at the front will be popped.</remarks>
+    public void PushBack(T? element)
+    {
+        if (IsFull)
+        {
+            Buffer[End] = element;
+            IncrementIndex(ref End);
+            Start = End;
+        }
+        else
+        {
+            Buffer[End] = element;
+            IncrementIndex(ref End);
+            Size++;
+        }
+    }
+
+    /// <summary>Removes the element at the front of the buffer.</summary>
+    /// <remarks>This decrements <see cref="Size"/> by one.</remarks>
+    public void PopFront()
+    {
+        if (IsEmpty)
+            throw new InvalidOperationException("Cannot remove an element from an empty buffer.");
+
+        Buffer[Start] = default;
+        IncrementIndex(ref Start);
+        Size--;
+    }
+
+    /// <summary>Removes the element at the end of the buffer.</summary>
+    /// <remarks>This decrements <see cref="Size"/> by one.</remarks>
+    public void PopBack()
+    {
+        if (IsEmpty)
+            throw new InvalidOperationException("Cannot remove an element from an empty buffer.");
+
+        DecrementIndex(ref End);
+        Buffer[End] = default;
+        Size--;
+    }
+
+    /// <summary>Clears the contents of the buffer.</summary>
+    public void Clear()
+    {
+        Start = 0;
+        End = 0;
+        Size = 0;
+        Array.Clear(Buffer);
+    }
+
+    /// <summary>Copies the contents of the buffer to an array.</summary>
+    /// <returns>A new array with the content of the buffer.</returns>
+    public T?[] ToArray()
+    {
+        var array = new T?[Size];
+        var arrayOffset = 0;
+
+        foreach (var segment in GetArraySegments())
+        {
+            Array.Copy(segment.Array!, segment.Offset, array, arrayOffset, segment.Count);
+            arrayOffset += segment.Count;
+        }
+
+        return array;
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<T?> GetEnumerator()
+    {
+        foreach (var segment in GetArraySegments())
+            for (int i = 0; i < segment.Count; i++)
+                yield return segment.Array![segment.Offset + i];
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+
+    /*********
+    ** Private Methods
+    *********/
+    /// <summary>Converts a index for the circular buffer to an index for <see cref="Buffer"/>.</summary>
+    /// <param name="index">The index of the circular buffer.</param>
+    /// <returns>The index for <see cref="Buffer"/>.</returns>
+    private int ConvertToInternalIndex(int index) => Start + (index < (Capacity - Start) ? index : index - Capacity);
+
+    /// <summary>Increments an index by one, wrapping the index if necessary.</summary>
+    /// <param name="index">The index to increment.</param>
+    private void IncrementIndex(ref int index)
+    {
+        if (++index == Capacity)
+            index = 0;
+    }
+
+    /// <summary>Decrements an index by one, wrapping the index if necessary.</summary>
+    /// <param name="index">The index to decrement.</param>
+    private void DecrementIndex(ref int index)
+    {
+        if (--index < 0)
+            index = Capacity - 1;
+    }
+
+    /// <summary>Gets the two array segments that can be used to convert the circular buffer to a linear buffer.</summary>
+    /// <returns>The two array segments that represent the buffer.</returns>
+    private ArraySegment<T?>[] GetArraySegments()
+    {
+        var arraySegments = new ArraySegment<T?>[2];
+
+        if (IsEmpty)
+        {
+            arraySegments[0] = new(Array.Empty<T>());
+            arraySegments[1] = new(Array.Empty<T>());
+        }
+        else if (Start < End)
+        {
+            arraySegments[0] = new(Buffer, Start, End - Start);
+            arraySegments[1] = new(Buffer, End, 0);
+        }
+        else
+        {
+            arraySegments[0] = new(Buffer, Start, Buffer.Length - Start);
+            arraySegments[1] = new(Buffer, 0, End);
+        }
+
+        return arraySegments;
+    }
+}
