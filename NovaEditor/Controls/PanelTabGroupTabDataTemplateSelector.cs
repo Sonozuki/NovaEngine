4bcdcf6e699f9f1545ab42eaeb2870a2c3b248d9
Andy Crocker <atmcrocker@gmail.com>
2023-04-02 21:03:18 +0100

    Add panel styling for editor

---
+++ NovaEditor/Controls/PanelTabGroupTabDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿namespace NovaEditor.Controls;
+
+/// <summary>The template selector for a tab in a <see cref="PanelTabGroup"/>.</summary>
+internal sealed class PanelTabGroupTabDataTemplateSelector : DataTemplateSelector
+{
+    /*********
+    ** Properties
+    *********/
+    /// <summary>The template for an active tab.</summary>
+    public DataTemplate ActiveTemplate { get; set; }
+
+    /// <summary>The template for an inactive tab.</summary>
+    public DataTemplate InactiveTemplate { get; set; }
+
+
+    /*********
+    ** Protected Methods
+    *********/
+    /// <inheritdoc/>
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        if (item == ((PanelTabGroupViewModel)container.BindingContext).ActivePanel)
+            return ActiveTemplate;
+        else
+            return InactiveTemplate;
+    }
+}
--- NovaEditor/ViewModels/PanelTabGroupViewModel.cs
+++ NovaEditor/ViewModels/PanelTabGroupViewModel.cs
@@ -22,4 +22,31 @@ internal sealed class PanelTabGroupViewModel : BindableObject
 
     /// <summary>The panels in the group.</summary>
     public ObservableCollection<PanelBase> Panels { get; } = new();
+
+    /// <summary>The command used to change the active tab.</summary>
+    public ICommand ChangeActiveTabCommand { get; set; }
+
+
+    /*********
+    ** Constructors
+    *********/
+    /// <summary>Constructs an instance.</summary>
+    public PanelTabGroupViewModel()
+    {
+        ChangeActiveTabCommand = new RelayCommand<PanelBase>(ChangeActiveTab);
+    }
+
+
+    /*********
+    ** Private Methods
+    *********/
+    /// <summary>Changes the active tab of the group.</summary>
+    /// <param name="panel">The panel to set as the active panel.</param>
+    private void ChangeActiveTab(PanelBase panel)
+    {
+        ActivePanel = panel;
+
+        // resetting the collection is required so the UI rerenders the tabs to reset the style for the active tab
+        Panels.GetType().GetMethod("OnCollectionReset", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(Panels, null);
+    }
 }
