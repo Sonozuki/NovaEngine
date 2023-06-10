b4547bb583c92d9b4f945cf176817f0aea354861
Andy Crocker <atmcrocker@gmail.com>
2023-06-10 20:16:36 +0100

    Remove folder svg icon, add folder and file xaml icons

---
+++ NovaEditor/Controls/Icons/FileIcon.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace NovaEditor.Controls.Icons;
+
+/// <summary>Represents the icon for files in <see cref="AssetsPanel"/>.</summary>
+public partial class FileIcon : UserControl
+{
+    /*********
+    ** Constructors
+    *********/
+    /// <summary>Constructs an instance.</summary>
+    public FileIcon()
+    {
+        InitializeComponent();
+    }
+}
---
+++ NovaEditor/Controls/Icons/FolderIcon.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace NovaEditor.Controls.Icons;
+
+/// <summary>Represents the icon for folders in <see cref="AssetsPanel"/>.</summary>
+public partial class FolderIcon : UserControl
+{
+    /*********
+    ** Constructors
+    *********/
+    /// <summary>Constructs an instance.</summary>
+    public FolderIcon()
+    {
+        InitializeComponent();
+    }
+}
