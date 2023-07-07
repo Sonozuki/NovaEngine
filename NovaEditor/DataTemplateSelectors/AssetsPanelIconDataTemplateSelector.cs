6d5153b257eee45ae72bbb2c4e04631b4d03c687
Andy Crocker <atmcrocker@gmail.com>
2023-07-07 05:02:01 +0100

    Add caching for templates in AssetsPanelIconDataTemplateSelector

--- NovaEditor/DataTemplateSelectors/AssetsPanelIconDataTemplateSelector.cs
+++ NovaEditor/DataTemplateSelectors/AssetsPanelIconDataTemplateSelector.cs
@@ -4,14 +4,22 @@
 public class AssetsPanelIconDataTemplateSelector : DataTemplateSelector
 {
     /*********
+    ** Fields
+    *********/
+    /// <summary>The template for the asset folder icon.</summary>
+    private readonly DataTemplate AssetFolderIconTemplate = (DataTemplate)Application.Current.FindResource("AssetFolderIcon");
+
+    /// <summary>The template for the asset file icon.</summary>
+    private readonly DataTemplate AssetFileIconTemplate = (DataTemplate)Application.Current.FindResource("AssetFileIcon");
+
+
+    /*********
     ** Public Methods
     *********/
     /// <inheritdoc/>
     public override DataTemplate SelectTemplate(object item, DependencyObject container)
     {
         ArgumentNullException.ThrowIfNull(item);
-
-        var pathInfo = (PathInfo)item;
-        return (DataTemplate)Application.Current.FindResource(pathInfo.IsDirectory ? "AssetFolderIcon" : "AssetFileIcon");
+        return ((PathInfo)item).IsDirectory ? AssetFolderIconTemplate : AssetFileIconTemplate;
     }
 }
