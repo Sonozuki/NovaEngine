90c933641434c088a1f64ecf4799002708ed5ba2
Andy Crocker <atmcrocker@gmail.com>
2023-08-18 13:14:29 +0100

    Add initial hierarchy panel scene tree view

--- NovaEditor/Controls/HierarchyPanel.xaml.cs
+++ NovaEditor/Controls/HierarchyPanel.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace NovaEditor.Controls;
+﻿using SceneManager = NovaEditor.Managers.SceneManager;
+
+namespace NovaEditor.Controls;
 
 /// <summary>Represents the panel used for managing the content of scenes.</summary>
 public partial class HierarchyPanel : EditorPanelBase
@@ -10,5 +12,32 @@ public partial class HierarchyPanel : EditorPanelBase
     public HierarchyPanel()
     {
         InitializeComponent();
+
+        // TODO: there may also need to be some sort of dependency system, e.g. if scene X is loaded which requires the HUD scene, the HUD
+        // scene will automatically be loaded this needs some way of setting ofc and to be displayed. perhaps show all scenes in this panel
+        // and you can select scenes to hide all others that aren't that scene or any of it's dependency scenes
+        foreach (var scene in SceneManager.AllScenes)
+            RootTreeView.Items.Add(CreateTreeViewItem(scene.Name, scene.RootGameObjects));
+    }
+
+
+    /*********
+    ** Private Methods
+    *********/
+    /// <summary>Creates a <see cref="TreeViewItem"/> with a name and child game objects.</summary>
+    /// <param name="name">The header of the <see cref="TreeViewItem"/>.</param>
+    /// <param name="childGameObjects">The children of the <see cref="TreeViewItem"/>.</param>
+    /// <returns>The <see cref="TreeViewItem"/> with the specified name and children.</returns>
+    private TreeViewItem CreateTreeViewItem(string name, IEnumerable<GameObject> childGameObjects)
+    {
+        var treeViewItem = new TreeViewItem
+        {
+            Header = name
+        };
+
+        foreach (var childGameObject in childGameObjects)
+            treeViewItem.Items.Add(CreateTreeViewItem(childGameObject.Name, childGameObject.Children));
+
+        return treeViewItem;
     }
 }
