bcb41537013b693d09b37ec1da46c9a12a53cd61
Andy Crocker <atmcrocker@gmail.com>
2024-01-18 01:51:55 +0000

    Add label and Vector3 block controls

---
+++ NovaEditor/Controls/Label.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace NovaEditor.Controls;
+
+/// <summary>Represents a control that can display a <see langword="string"/>.</summary>
+public partial class Label : UserControl
+{
+    /*********
+    ** Fields
+    *********/
+    /// <summary>The text of the label.</summary>
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(Label));
+
+
+    /*********
+    ** Properties
+    *********/
+    /// <summary>The text of the label.</summary>
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+
+    /*********
+    ** Constructors
+    *********/
+    /// <summary>Constructs an instance.</summary>
+    public Label()
+    {
+        InitializeComponent();
+    }
+}
---
+++ NovaEditor/Controls/Vector3FloatBlock.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace NovaEditor.Controls;
+
+/// <summary>Represents a control that can display or edit a <see cref="Vector3{T}"/> (float).</summary>
+public partial class Vector3FloatBlock : UserControl
+{
+    /*********
+    ** Constructors
+    *********/
+    /// <summary>Constructs an instance.</summary>
+    /// <param name="getValue">The function to call to retrieve the <see cref="Vector3{T}"/> (float) being bound.</param>
+    /// <param name="setValue">The function to call to set the <see cref="Vector3{T}"/> (float) being bound.</param>
+    public Vector3FloatBlock(Func<Vector3<float>> getValue, Action<Vector3<float>> setValue)
+    {
+        InitializeComponent();
+
+        RootStackPanel.Children.Add(new FloatBox(() => getValue().X, value => {
+            var vector = getValue();
+            vector.X = value;
+            setValue(vector);
+        }));
+
+        RootStackPanel.Children.Add(new FloatBox(() => getValue().Y, value => {
+            var vector = getValue();
+            vector.Y = value;
+            setValue(vector);
+        }));
+
+        RootStackPanel.Children.Add(new FloatBox(() => getValue().Z, value => {
+            var vector = getValue();
+            vector.Z = value;
+            setValue(vector);
+        }));
+    }
+}
